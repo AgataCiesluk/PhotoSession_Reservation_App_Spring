f7ac29cb17a6907e78ab5af1ed85186c94481f86
Agata Ciesluk <agata.mikla@gmail.com>
2022-05-21 11:39:59 +0200

    Project creation.

---
+++ src/main/java/com/github/agataciesluk/photosession_reservation_app_spring/PhotoSessionReservationAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.agataciesluk.photosession_reservation_app_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoSessionReservationAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoSessionReservationAppSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/agataciesluk/photosession_reservation_app_spring/PhotoSessionReservationAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.agataciesluk.photosession_reservation_app_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoSessionReservationAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
